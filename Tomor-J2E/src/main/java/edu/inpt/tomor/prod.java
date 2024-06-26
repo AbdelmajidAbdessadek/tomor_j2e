c6e19758c58b453c9a15781561cdeb20abdae589
tahaakml <tk93862@gmail.com>
2024-06-26 18:19:06 +0100

    product.java

---
+++ Tomor-J2E/src/main/java/edu/inpt/tomor/prod.java
@@ -0,0 +1,94 @@
+package edu.inpt.tomor;
+
+public class Product {
+    private int productId;
+    private String productName;
+    private String category;
+    private double price;
+    private String description;
+    private int stockQuantity;
+    private String imageUrl;
+
+    public Product() {
+    }
+
+    public Product(int productId, String productName, String category, double price, String description,
+            int stockQuantity, String imageUrl) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+        this.stockQuantity = stockQuantity;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductBean{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", stockQuantity=" + stockQuantity +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
